617649af0c265632eef084cfc7deecdee0486e60
Manali Mahesh Kadam <manalikadam051@gmail.com>
2024-01-28 12:21:21 +0530

    Portfolio

---
+++ Desktop/Manali Kadam/assets/index.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["Web Developer","Web Designer","Frontend Developer","Learner"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
